36b254aaf89b7bb9a90b3c58a53e6383b4bab838
zabbixserver <videomc1979@gmail.com>
2024-04-16 23:22:43 +0300

    add files tasks.py

--- fan_board/tasks.py
+++ fan_board/tasks.py
@@ -1,3 +1,5 @@
+from datetime import timedelta, date
+
 from celery import shared_task
 from django.core.mail import send_mail
 from django.utils import timezone
@@ -8,29 +10,40 @@ from users.models import CustomUser
 
 @shared_task
 def send_weekly_newsletter():
-    # Определите категории объявлений, которые вы хотите включить в рассылку
-    categories = ['Недвижимость', 'Транспорт', 'Работа']  # Пример категорий
+    week = timedelta(days=7)
+    ads = Advertisement.objects.all()
+    advertisement_week_ads = []
+    template = 'weekly_digest.html'
+    email_subject = 'Your News Portal Weekly Digest'
 
-    # Определите дату начала недели
-    start_date = timezone.now() - timezone.timedelta(days=7)
+    for ad in ads:
+        if ad.created_at >= date.today() - week:
+            advertisement_week_ads.append(ad)
 
-    # Получите все объявления за последнюю неделю в указанных категориях
-    new_ads = Advertisement.objects.filter(category__in=categories, created_at__gte=start_date)
 
-    # Получите список зарегистрированных пользователей
-    users = CustomUser.objects.all()
-
-    for user in users:
-        # Формируем контент для письма
-        context = {
-            'user': user,
-            'new_ads': new_ads,
-        }
-
-        # Отправляем письмо
-        subject = 'Еженедельная рассылка новостей'
-        message = render_to_string('notification_email.html', context)
-        user_email = user.email
-        send_mail(subject, message, 'noreply@example.com', [user_email])
-
-    print("Weekly newsletter sent successfully!")
+    # # Определите категории объявлений, которые вы хотите включить в рассылку
+    # categories = ['Недвижимость', 'Транспорт', 'Работа']  # Пример категорий
+    #
+    # # Определите дату начала недели
+    # start_date = timezone.now() - timezone.timedelta(days=7)
+    #
+    # # Получите все объявления за последнюю неделю в указанных категориях
+    # new_ads = Advertisement.objects.filter(category__in=categories, created_at__gte=start_date)
+    #
+    # # Получите список зарегистрированных пользователей
+    # users = CustomUser.objects.all()
+    #
+    # for user in users:
+    #     # Формируем контент для письма
+    #     context = {
+    #         'user': user,
+    #         'new_ads': new_ads,
+    #     }
+    #
+    #     # Отправляем письмо
+    #     subject = 'Еженедельная рассылка новостей'
+    #     message = render_to_string('notification_email.html', context)
+    #     user_email = user.email
+    #     send_mail(subject, message, 'noreply@example.com', [user_email])
+    #
+    # print("Weekly newsletter sent successfully!")
